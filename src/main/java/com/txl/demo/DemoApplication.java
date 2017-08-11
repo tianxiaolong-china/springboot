e192574db3883bb2acb03cc287942453d1537cdd
tianxiaolong-china <444363699@qq.com>
2017-08-11 08:06:49 +0800

    整合restTemplate 和 redis

--- src/main/java/com/txl/demo/DemoApplication.java
+++ src/main/java/com/txl/demo/DemoApplication.java
@@ -3,8 +3,11 @@ package com.txl.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication()
+@ImportResource(locations={"classpath:spring/spring-bean.xml"})
 @ServletComponentScan
 public class DemoApplication {
 
--- src/main/java/com/txl/demo/controller/HelloController.java
+++ src/main/java/com/txl/demo/controller/HelloController.java
@@ -5,10 +5,14 @@ import com.txl.demo.dao.GroupInfo;
 import com.txl.demo.dao.GroupInfoDao;
 import com.txl.demo.version.ApiVersion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by TeaBee on 2017/7/27.
@@ -22,47 +26,61 @@ public class HelloController {
     @Autowired
     private GroupInfoDao groupInfoDao;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @RequestMapping(method = RequestMethod.GET , value = "/mybatis")
+    public Object getMybatis(){
+        GroupInfo query = groupInfoDao.query("2");
+        GroupInfo groupInfo = new GroupInfo();
+        groupInfo.setGroupId("2");
+        groupInfo.setGroupName("bbb");
+        groupInfo.setFilePath("ddd");
+        groupInfoDao.delete("2");
+        groupInfoDao.insert(groupInfo);
+
+        groupInfo.setGroupName("txl");
+        groupInfoDao.update(groupInfo);
+
+        restConnection.connect();
+        return query;
+    }
+
     @RequestMapping(method = RequestMethod.GET , value = "/hello")
-    @ApiVersion(1.1)
     public Object getHello(){
-        GroupInfo query = groupInfoDao.query("2");
-        GroupInfo groupInfo = new GroupInfo();
-        groupInfo.setGroupId("2");
-        groupInfo.setGroupName("bbb");
-        groupInfo.setFilePath("ddd");
-        groupInfoDao.delete("2");
-        groupInfoDao.insert(groupInfo);
-
-        groupInfo.setGroupName("txl");
-        groupInfoDao.update(groupInfo);
-
-        restConnection.connect();
-        return query;
+        return "Hello";
     }
 
-    @RequestMapping(method = RequestMethod.GET , value = "/hello")
-    @ApiVersion(2)
-    public Object getHelloV2(){
-        GroupInfo query = groupInfoDao.query("2");
-        GroupInfo groupInfo = new GroupInfo();
-        groupInfo.setGroupId("2");
-        groupInfo.setGroupName("bbb");
-        groupInfo.setFilePath("ddd");
-        groupInfoDao.delete("2");
-        groupInfoDao.insert(groupInfo);
-
-        groupInfo.setGroupName("txl");
-        groupInfoDao.update(groupInfo);
-
-        restConnection.connect();
-        return query;
+    @RequestMapping(method = RequestMethod.GET , value = "/rest")
+    public Object getRest(){
+        ResponseEntity<Object> entity = restTemplate.getForEntity("http://api.avatardata.cn/MobilePlace/LookUp?key=023d7e6345e341feabee16dbb458eec0&mobileNumber=13021671512", Object.class);
+        Object body = entity.getBody();
+        return body;
     }
 
-    @RequestMapping(method = RequestMethod.GET , value = "/hello")
-    @ApiVersion(value = 2.3,isUsing = true, expireDate = "2018-5-1 0:0:0")
-    public Object getHelloV3(){
-        return "hello";
+    @RequestMapping(method = RequestMethod.GET , value = "/redisSet")
+    public Object getRedis(){
+
+        redisTemplate.opsForValue().set("tianxiaolong","salory:15000");
+        return "Get";
     }
+
+    @RequestMapping(method = RequestMethod.GET , value = "/redisGet")
+    public Object getRedisGet(){
+
+        if (redisTemplate.hasKey("tianxiaolong")){
+            Object tianxiaolong = redisTemplate.opsForValue().get("tianxiaolong");
+            return tianxiaolong;
+        }
+        return "failed";
+    }
+
 
     @RequestMapping(method = RequestMethod.GET , value = "/test")
     @ApiVersion(1.3)
